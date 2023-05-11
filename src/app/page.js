a38a53250a8734a2762ee6ccf755cec7bc3997b8
emrecoban <fbdarktole@gmail.com>
2023-05-11 17:32:38 +0300

    Added Twitter API SDK

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,13 @@
-export default function TwitterGaller() {
-  return <h1>selam</h1>
+import { Client } from "twitter-api-sdk";
+
+const client = new Client(process.env.BEARER_TOKEN);
+
+export default async function TwitterGallery() {
+  const gelen = await client.users.findUserByUsername("emreshepherd")
+  return (
+    <>
+      <h1>selam</h1>
+      <pre>{JSON.stringify(gelen, 2, null)}</pre>
+    </>
+  )
 }
