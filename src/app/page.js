6d82d04d14a39637183e2a1b9c8f7cf4485a7984
emrecoban <fbdarktole@gmail.com>
2023-05-11 17:39:19 +0300

    getting tweets from username

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,14 @@ import { Client } from "twitter-api-sdk";
 const client = new Client(process.env.BEARER_TOKEN);
 
 export default async function TwitterGallery() {
-  const gelen = await client.users.findUserByUsername("emreshepherd")
+  const userId = await client.users.findUserByUsername("emreshepherd")
+  const tweets = await client.tweets.usersIdTweets(userId.data.id)
+
   return (
     <>
       <h1>selam</h1>
-      <pre>{JSON.stringify(gelen, 2, null)}</pre>
+      <pre>{JSON.stringify(userId, 2, null)}</pre>
+      <pre>{JSON.stringify(tweets, 2, null)}</pre>
     </>
   )
 }
